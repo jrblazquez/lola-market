0eb2c7fc0e7fc3a6c445f2b9eacc342e598fe7e9
jrblazquez <jrblazquez@gmail.com>
2019-03-06 16:11:07 +0100

    023: Created not found page

---
+++ src/app/components/notFound/components/styles.js
@@ -0,0 +1,13 @@
+const styles = theme => ({
+  container: {
+    textAlign: 'center',
+    margin: '10rem 2rem',
+    fontSize: '1.5rem',
+    lineHeight: 1.4,
+  },
+  headersIconsSeparation: {
+    border: '1px solid white',
+  },
+});
+
+export default styles;
--- src/app/index.js
+++ src/app/index.js
@@ -1,22 +1,21 @@
 
 import { connect } from 'react-redux';
 import { types as TYPES } from 'store/location';
-import {NOT_FOUND} from 'redux-first-router';
 import App from './components';
 import Market from './components/market';
 import Category from './components/category';
 import Subcategory from './components/subcategory';
+import NotFound from './components/notFound';
 
 const routesMap = {
-  [TYPES.GO_HOME]: Market,
+  [TYPES.GO_HOME]: NotFound,
   [TYPES.GO_MARKET]: Market,
   [TYPES.GO_MARKET_CATEGORY]: Category,
   [TYPES.GO_MARKET_CATEGORY_SUBCATEGORY]: Subcategory,
-  NOT_FOUND: Market,
 };
 
 const mapStateToProps = state => ({
-  Component: routesMap[state.location.type] ? routesMap[state.location.type] : routesMap[NOT_FOUND],
+  Component: routesMap[state.location.type] ? routesMap[state.location.type] : NotFound,
 });
 
 export default connect(mapStateToProps)(App);
